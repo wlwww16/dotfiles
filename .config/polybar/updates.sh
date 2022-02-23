fe3189084ace1911460c816e5eb794e9bda9935b
wlw-codes <will.lewis.w@gmail.com>
2022-02-23 01:01:39 +0000

    polybar updates

--- .config/polybar/updates.sh
+++ .config/polybar/updates.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-for a in $(ls /bin); do
-	case $a in
-		pacman) b=$(pacman -Qu | wc -l);;
-	esac
-done
+# This script will return the number of package updates
+# available at any one time, via pacman.
+#
+# Superuser commands are not ran in this script, but
+# pacman needs superuser to sync repositories (and
+# therefore get updates).
+#
+# To solve this, run pacman -Sy as a cronjob every n
+# minutes, where n is something less than this module's
+# interval time.
 
-if [[ "$b" -ne "0" ]]; then
-	echo $b;
+# Get a list of package updates, count number of lines
+updates=$(pacman -Qu | wc -l)
+
+# If number of updates is not equal to zero...
+if [[ "$updates" -ne "0" ]]; then
+	echo $updates;
 fi
